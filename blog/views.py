4e447f3b2f9165ff335b9e323e99f007296ca479
Dylan Swanson <81135334+Daswanson22@users.noreply.github.com>
2025-03-26 17:38:52 -0700

    Post can be filtered by pub date

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,12 @@ from django.utils import timezone
 from .models import Post
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    query = request.GET.get('q')
+    post = None
+    if query and query == 'oldest':
+        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    else:
+        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {
         'posts': posts
     })
